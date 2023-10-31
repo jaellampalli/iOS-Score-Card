3b2931848760a298317fea5ced230312ec86789a
Zackary Crow <zackcrow@rocketmail.com>
2023-10-30 22:47:16 -0400

    establish custom event to poll and send ws data

--- backend/backend/consumers.py
+++ backend/backend/consumers.py
@@ -1,7 +1,14 @@
 from channels.generic.websocket import WebsocketConsumer
+import json
 
 class CameraConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
+        self.send(text_data=json.dumps({
+            'type':'connection_established',
+            'message':'poggers, connected'}))
+    def receive(self, text_data= None):
+        print(json.dumps(text_data))
+    
     def disconnect(self, close_code):
         pass
